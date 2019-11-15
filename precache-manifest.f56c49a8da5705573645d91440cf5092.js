d90acdea4e677be9cec5896e39edeabd1b126f7e
Maciek <mdrzewiecki56@gmail.com>
2019-11-15 14:52:44 +0100

    Updates

---
+++ precache-manifest.f56c49a8da5705573645d91440cf5092.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae0cec83c0128f688a110c9c3b7b237f",
+    "url": "/website/index.html"
+  },
+  {
+    "revision": "42196e8a18744035b940",
+    "url": "/website/static/css/2.833dd627.chunk.css"
+  },
+  {
+    "revision": "b532f8f8a87e613d5f8a",
+    "url": "/website/static/css/main.5bbb401f.chunk.css"
+  },
+  {
+    "revision": "42196e8a18744035b940",
+    "url": "/website/static/js/2.24d88ba7.chunk.js"
+  },
+  {
+    "revision": "b532f8f8a87e613d5f8a",
+    "url": "/website/static/js/main.734d0641.chunk.js"
+  },
+  {
+    "revision": "64377c2dea863a1783ef",
+    "url": "/website/static/js/runtime-main.4cafdc12.js"
+  },
+  {
+    "revision": "50f02411d9ce84254232c052bd7ed0d9",
+    "url": "/website/static/media/dog1.50f02411.jpg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/website/static/media/logo.25bf045c.svg"
+  }
+]);
