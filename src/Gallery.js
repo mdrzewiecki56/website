dfa580926a21d4e1e57539086c94f0fb17535397
Maciek <mdrzewiecki56@gmail.com>
2019-11-21 14:36:51 +0100

    responsive-fix, some styling

--- src/Gallery.js
+++ src/Gallery.js
@@ -38,14 +38,37 @@ class Gallery extends Component {
                 imgs.push([]);
             for (let j=0;j<3;j++){
                 let id = uuid();
-                if (j===1){
-                    imgs[i].push(<div key={id} className="col-lg-6 col-sm-12 col-centered"><Link to={`/img${id}`}><Image src={this.state.images[img_index].url} id={id}/></Link></div>);
-                    img_index++;
+                if (i%3===0){
+                    if (j===1){
+                        imgs[i].push(<Image src={this.state.images[img_index].url} id={id} classes="col-lg-6 col-sm-12 col-centered img-fluid app-img" key={id}/>);
+                        img_index++;
+                    }
+                    else{
+                        imgs[i].push(<Image src={this.state.images[img_index].url} id={id}  classes="col-lg-3 col-sm-12 col-centered img-fluid app-img" key={id}/>);
+                        img_index++;
+                    }   
                 }
-                else{
-                    imgs[i].push(<div key={id} className="col-lg-3 col-sm-12 col-centered"><Link to={`/img${id}`}><Image src={this.state.images[img_index].url} id={id}/></Link></div>);
-                    img_index++;
-                }   
+                if (i%3===1){
+                    if (j===1 || j===2){
+                        imgs[i].push(<Image src={this.state.images[img_index].url} id={id} classes="col-lg-3 col-sm-12 col-centered img-fluid app-img" key={id}/>);
+                        img_index++;
+                    }
+                    else{
+                        imgs[i].push(<Image src={this.state.images[img_index].url} id={id}  classes="col-lg-6 col-sm-12 col-centered img-fluid app-img" key={id}/>);
+                        img_index++;
+                    }    
+                }
+                if (i%3===2){
+                    if (j===2){
+                        imgs[i].push(<Image src={this.state.images[img_index].url} id={id} classes="col-lg-6 col-sm-12 col-centered img-fluid app-img" key={id}/>);
+                        img_index++;
+                    }
+                    else{
+                        imgs[i].push(<Image src={this.state.images[img_index].url} id={id}  classes="col-lg-3 col-sm-12 col-centered img-fluid app-img" key={id}/>);
+                        img_index++;
+                    }    
+                }
+
                     
             }
         }
@@ -53,7 +76,7 @@ class Gallery extends Component {
             <div className="Gallery pt-5">
                 {this.state.isLoading ? <div className="col-lg-12 col-centered"><Loader type="Puff" color="#00BFFF" height={250} width={250}/></div> : 
                 imgs.map(el => {let id = uuid(); return <div key={id} className="row">{el}</div>})}
-                {!this.state.isLoading && <button onClick={this.getImages}>Get More!</button>}
+                {!this.state.isLoading && <div className="row mt-3 mb-3"><div className="col-lg-4 col-md-3 col-sm-2"></div><button onClick={this.getImages} className="col-lg-4 col-md-6 col-sm-8 load">Get More</button><div className="col-lg-4 col-md-3 col-sm-2"></div></div>}
             </div>
 
         );
--- src/Image.js
+++ src/Image.js
@@ -4,7 +4,7 @@ import React, {Component} from 'react';
 class Image extends Component {
     render(){
         return (
-            <img src={this.props.src} alt={this.props.id} className="app-img mb-5"/>
+           <img src={this.props.src} alt={this.props.id} className={`${this.props.classes} img-fluid`}/>
         );
     }
 }
