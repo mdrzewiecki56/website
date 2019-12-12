d46d336b51fe14204df52a2ac1668d53136a254e
Maciek <mdrzewiecki56@gmail.com>
2019-12-12 14:23:22 +0100

    Update About.js

--- src/About.js
+++ src/About.js
@@ -9,15 +9,11 @@ class About extends Component {
                 <div className="row mx-auto mb-5">
                     <div className="col-lg-6 col-sm-12 col-centered "><img src={dog} alt="about" className="img-fluid" /></div> 
                     <div className="col-lg-5 offset-lg-1 col-sm-12 col-centered pt-2">
-                        <h3>Every component still needs some styling</h3>
+                        <h3 className="mb-4">Page under development</h3>
                         <p>Work in progress</p>
-                    </div>
-                </div>
-                <div className="row mx-auto mb-5">
-                    <div className="col-lg-6 col-sm-12 col-centered "><img src={dog} alt="about" className="img-fluid" /></div> 
-                    <div className="col-lg-5 offset-lg-1 col-sm-12 col-centered pt-2">
-                        <h3>Every component still needs some styling</h3>
-                        <p>Work in progress</p>
+                        <p>Made with ReactJS, routed with react-router, styled with SCSS</p>
+                        <p>Gallery - bootstrap, react-awesome-spinners, axios</p>
+                        <p>ToDo List - css flexbox</p>
                     </div>
                 </div>
             </div>
