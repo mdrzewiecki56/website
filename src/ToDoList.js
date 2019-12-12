b2d8d96c26d188edf4939c6ced97c07d70a85c91
Maciek <mdrzewiecki56@gmail.com>
2019-12-12 13:56:20 +0100

    local storage added, some styling

--- src/ToDoList.js
+++ src/ToDoList.js
@@ -5,8 +5,7 @@ import './ToDoList.scss'
 export default class ToDoList extends Component {
     constructor(props) {
         super(props);
-        let id = uuid();
-        this.state = {tasks: [{text:"Walk a dog", id:id, key:id}], newTask:"Do shopping"};
+        this.state = {tasks: JSON.parse(window.localStorage.getItem("tasks") || "[]"), newTask: "Walk a dog"};
         this.handleChange = this.handleChange.bind(this);
         this.handleNewTask = this.handleNewTask.bind(this);
         this.updateTask = this.updateTask.bind(this);
@@ -23,8 +22,12 @@ export default class ToDoList extends Component {
         let newTask = {text: this.state.newTask, id:id};
         if(newTask.text === "")
             alert("Don't add empty tasks! :(");
-        else
-            this.setState({tasks: [...this.state.tasks, newTask], newTask:""});
+        else{
+            this.setState(st => ({
+                tasks: [...st.tasks, newTask], newTask: ""
+            }),()=>window.localStorage.setItem("tasks", JSON.stringify(this.state.tasks)));
+
+        }
     }
 
     updateTask(id, updatedTask){
@@ -33,25 +36,35 @@ export default class ToDoList extends Component {
                 return {...task, text: updatedTask}
             return task;
         });
-        this.setState({tasks: updatedList});  
+        this.setState({tasks: updatedList},()=>window.localStorage.setItem("tasks", JSON.stringify(this.state.tasks)));  
     }
 
     deleteTask(id){
         let taskList = this.state.tasks.filter(t => t.id !== id);
-        this.setState({tasks: taskList});
+        this.setState({tasks: taskList},()=>window.localStorage.setItem("tasks", JSON.stringify(this.state.tasks)));
     }
 
     render() {
-        let tasks = this.state.tasks.map((t)=>{
-            return <Task text={t.text} id={t.id} key={t.id} update={this.updateTask} delete={this.deleteTask} />
-        })
+        let tasks;
+        try{
+                tasks = this.state.tasks.map((t)=>{
+                return <Task text={t.text} id={t.id} key={t.id} update={this.updateTask} delete={this.deleteTask} />
+            })
+        }
+        catch{
+            localStorage.clear();
+        }
+
         return (
-            <div className="ToDoList">
+            <div className="ToDoList fadeIn">
                 <div className="list-container">
                     <div className="list-tasks">
+                        <div className="list-header">
+                            To-Do List
+                        </div>
                         {tasks}
                     </div>
-                    <form onSubmit={this.handleNewTask}>
+                    <form onSubmit={this.handleNewTask} className="taskForm">
                         <input placeholder="Do shopping" name="newTask" value={this.state.newTask} onChange={this.handleChange} ></input>
                         <button>Add!</button>
                     </form>
