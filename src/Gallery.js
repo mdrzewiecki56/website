6649d3b8e83dbbb911362c22a68466e83a022d87
Maciek <mdrzewiecki56@gmail.com>
2020-01-09 14:56:45 +0100

    gallery image preview

--- src/Gallery.js
+++ src/Gallery.js
@@ -8,8 +8,9 @@ import { DualRing } from 'react-awesome-spinners'
 class Gallery extends Component {
     constructor(props){
         super(props);
-        this.state = {images: [], isLoading: true};
+        this.state = {images: [], isLoading: true, isDisplaying: false, activeImage: ""};
         this.getImages = this.getImages.bind(this);
+        this.display = this.display.bind(this);
     }
 
     async componentDidMount(){
@@ -29,6 +30,10 @@ class Gallery extends Component {
         this.setState({images: imgs}, () => this.setState({isLoading: false}));
     }
 
+    display(src){
+        this.setState({isDisplaying:true, activeImage: src});
+    }
+
     render(){
         let imgs = [];
         let img_index = 0;
@@ -38,31 +43,31 @@ class Gallery extends Component {
                 let id = uuid();
                 if (i%3===0){
                     if (j===1){
-                        imgs[i].push(<Image src={this.state.images[img_index].url} id={id} classes="col-lg-6 col-sm-12 col-centered img-fluid app-img" key={id}/>);
+                        imgs[i].push(<Image src={this.state.images[img_index].url} id={id} classes="col-lg-6 col-sm-12 col-centered img-fluid app-img" key={id} display={this.display}/>);
                         img_index++;
                     }
                     else{
-                        imgs[i].push(<Image src={this.state.images[img_index].url} id={id}  classes="col-lg-3 col-sm-12 col-centered img-fluid app-img" key={id}/>);
+                        imgs[i].push(<Image src={this.state.images[img_index].url} id={id}  classes="col-lg-3 col-sm-12 col-centered img-fluid app-img" key={id} display={this.display}/>);
                         img_index++;
                     }   
                 }
                 if (i%3===1){
                     if (j===1 || j===2){
-                        imgs[i].push(<Image src={this.state.images[img_index].url} id={id} classes="col-lg-3 col-sm-12 col-centered img-fluid app-img" key={id}/>);
+                        imgs[i].push(<Image src={this.state.images[img_index].url} id={id} classes="col-lg-3 col-sm-12 col-centered img-fluid app-img" key={id} display={this.display}/>);
                         img_index++;
                     }
                     else{
-                        imgs[i].push(<Image src={this.state.images[img_index].url} id={id}  classes="col-lg-6 col-sm-12 col-centered img-fluid app-img" key={id}/>);
+                        imgs[i].push(<Image src={this.state.images[img_index].url} id={id}  classes="col-lg-6 col-sm-12 col-centered img-fluid app-img" key={id} display={this.display}/>);
                         img_index++;
                     }    
                 }
                 if (i%3===2){
                     if (j===2){
-                        imgs[i].push(<Image src={this.state.images[img_index].url} id={id} classes="col-lg-6 col-sm-12 col-centered img-fluid app-img" key={id}/>);
+                        imgs[i].push(<Image src={this.state.images[img_index].url} id={id} classes="col-lg-6 col-sm-12 col-centered img-fluid app-img" key={id} display={this.display}/>);
                         img_index++;
                     }
                     else{
-                        imgs[i].push(<Image src={this.state.images[img_index].url} id={id}  classes="col-lg-3 col-sm-12 col-centered img-fluid app-img" key={id}/>);
+                        imgs[i].push(<Image src={this.state.images[img_index].url} id={id}  classes="col-lg-3 col-sm-12 col-centered img-fluid app-img" key={id} display={this.display}/>);
                         img_index++;
                     }    
                 }
@@ -70,11 +75,17 @@ class Gallery extends Component {
                     
             }
         }
+        let layout = 0;
+        if (this.state.isLoading)
+            layout = <div className="col-lg-12 col-centered loader"><DualRing size={15} sizeUnit={'vh'}/></div>;
+        else if (this.state.isDisplaying)
+            layout = <div className="overlay fadeIn" onClick={() => this.setState({isDisplaying: false, activeImage:""})}><img src={this.state.activeImage} alt="activeImage" className="img-fluid"/></div>;
+        else
+            layout = <div>{imgs.map(el => {let id = uuid(); return <div key={id} className="row fadeIn">{el}</div>})}
+            <div className="row mt-3 mb-3 fadeIn"><button onClick={this.getImages} className="load col-lg-6 offset-lg-3 col-sm-10 offset-sm-1">Get More</button></div></div>;
         return (
             <div className="Gallery pt-3 fadeIn">
-                {this.state.isLoading ? <div className="col-lg-12 col-centered loader"><DualRing size={15} sizeUnit={'vh'}/></div> : 
-                imgs.map(el => {let id = uuid(); return <div key={id} className="row fadeIn">{el}</div>})}
-                {!this.state.isLoading && <div className="row mt-3 mb-3 fadeIn"><button onClick={this.getImages} className="load col-lg-6 offset-lg-3 col-sm-10 offset-sm-1">Get More</button></div>}
+                {layout}
             </div>
 
         );
--- src/Image.js
+++ src/Image.js
@@ -4,7 +4,7 @@ import React, {Component} from 'react';
 class Image extends Component {
     render(){
         return (
-           <img src={this.props.src} alt={this.props.id} className={`${this.props.classes} img-fluid`}/>
+           <img src={this.props.src} alt={this.props.id} className={`${this.props.classes} img-fluid`} onClick={() => this.props.display(this.props.src)}/>
         );
     }
 }
